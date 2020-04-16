81748109232c657ac11edd82ff96f240289b44a6
Lucas Gabriel <lucas.gdsouza@outlook.com>
2020-04-15 22:44:11 -0300

    Initial Commit

---
+++ src/main/java/br/com/fiap/thebestway/TheBestWayApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.thebestway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBestWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBestWayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/thebestway/TheBestWayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.thebestway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheBestWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
