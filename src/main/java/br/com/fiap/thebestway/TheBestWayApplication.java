851a5d75bcc0e5f72289851a0a1609df2ac9ff86
Lucas Gabriel <lucas.gdsouza@outlook.com>
2020-04-19 12:54:10 -0300

    App starting with informations

--- src/main/java/br/com/fiap/thebestway/TheBestWayApplication.java
+++ src/main/java/br/com/fiap/thebestway/TheBestWayApplication.java
@@ -1,13 +1,34 @@
 package br.com.fiap.thebestway;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.fiap.thebestway.domain.Categoria;
+import br.com.fiap.thebestway.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class TheBestWayApplication {
+public class TheBestWayApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TheBestWayApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria categoria1 = new Categoria(null, "X");
+		Categoria categoria2 = new Categoria(null, "VIP");
+		Categoria categoria3 = new Categoria(null, "COMFORT");
+		Categoria categoria4 = new Categoria(null, "BAG");
+
+		repository.saveAll(Arrays.asList(categoria1, categoria2, categoria3, categoria4));
+
+	}
 }
